9714cf7b8e56460768c742b3098b8c239e60fe2f
Chris <44784345+chrisalexander0617@users.noreply.github.com>
2021-10-28 19:35:49 -0400

    Init commit

---
+++ script.js
@@ -0,0 +1,37 @@
+/* Buttons */
+const followButton = document.getElementById('follow-button');
+const messageButton = document.getElementById('message-button');
+
+/* Modal elements */
+const overlay = document.querySelector('.overlay');
+const modalWrapper = document.querySelector('.modal-wrapper');
+const modalCloseButton = document.getElementById('modal-close-button');
+
+/* Text nodes */
+const modalMessage = document.querySelector('.modal-message');
+const followMessageText = document.querySelector('.follow-message-text');
+
+
+
+followButton.addEventListener('click', () => showFollowMessageText('You are now following Marcus Doe!'));
+messageButton.addEventListener('click', () => openModal('This feature is not implemented yet!'));
+
+modalCloseButton.addEventListener('click',  () => {
+    overlay.classList.add('close')
+    modalWrapper.classList.add('close')
+});
+
+
+function showFollowMessageText(message){
+    followMessageText.innerHTML = message;
+    setTimeout(()=> {
+        followMessageText.innerHTML = ''
+    },3000)
+}
+
+function openModal(message){
+    overlay.classList.remove('close')
+    modalWrapper.classList.remove('close')
+    modalMessage.innerHTML = message;
+    modalWrapper.classList.add('animate__rubberBand');
+}
